8b28f10127cbd0c20c29e1921cbb9c24b7a8560a
Tony.Shao <xiocode@gmail.com>
2017-02-04 16:46:22 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ exec ss-server -s $SERVER_ADDR \
                -m $METHOD \
                -t $TIMEOUT \
                --fast-open \
-               -u
+               -u \
+               --plugin obfs-server --plugin-opts "${OBFS_OPTS}"
 
 
